99b832209e0c41ff7b0dcfa46e62a390866ce2d4
SohamAgawane <93815863+SohamAgawane@users.noreply.github.com>
2024-08-19 18:44:58 +0530

    git add

--- Basics/04-Array-Search.cpp
+++ Basics/04-Array-Search.cpp
@@ -1,15 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-   
 
-int search(int arr[], int N, int X)
-    {
-        
-        // Your code here
-        for(int i=0;i<N;i++){
-            if(arr[i] == X)
-            return i;
+int main(){
+    int arr[6] = {3, 8, 5, 9, 2, 7};
+
+    int n;
+    cin >> n;
+
+    for(int i=0; i<=6; i++){
+        if(arr[i] == n){
+            cout << "Present";
+            return 0;
         }
-        
-        return -1;
     }
+    cout << "Not present";
+}
