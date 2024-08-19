13d663fa788c8ec56e6ccd93ffa306955256f3dc
SohamAgawane <93815863+SohamAgawane@users.noreply.github.com>
2024-08-19 19:28:03 +0530

    git add

--- Basics/07-Missing-Number.cpp
+++ Basics/07-Missing-Number.cpp
@@ -1,24 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
-  public:
+int main(){
+    int arr[5] = {1, 2, 4, 5, 6};
+    
+    int n = 6;
+    
+    int sum = 0;
 
-    // Note that the size of the array is n-1
-    int missingNumber(int n, vector<int>& arr) {
-
-        // Sum of all elements in array :
-        int sum = 0;
-        
-        for(int i=0; i<arr.size(); i++){
-            sum += arr[i];
-        }
-        
-        
-        // Sum of n numbers :
-        int ans = n*(n+1)/2;
-        
-        return ans-sum;
-        
+    for(int i=0; i<5; i++){
+        sum += arr[i];
     }
-};
+
+    int expectedSum = n * (n + 1) / 2;
+
+    int missingNumber = expectedSum - sum;
+
+    cout << missingNumber; 
+}
